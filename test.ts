215e5763ff4a8305cc7c67b159af9e9fb2bd317a
Isaac Ezer <iezer@justin.tv>
2016-09-14 20:26:15 -0400

    twitch-config

---
+++ test.ts
@@ -0,0 +1,79 @@
+import { InitialRenderBenchmark, InitialRenderSamples, ITab, Runner } from "chrome-tracing";
+import * as fs from "fs";
+
+const DEFAULT_PORT = 9292;
+
+let browserOpts = process.env.CHROME_BIN ? {
+  type: "exact",
+  executablePath: process.env.CHROME_BIN
+} : {
+  type: "system"
+};
+
+const DATA = {
+  "GET http://my.api.com/posts": {
+    "posts": [
+      {
+        "id": 1,
+        "title": "Rails is omakase",
+        "body": "There are lots of à la carte software..."
+      },
+      {
+        "id": 2,
+        "title": "Ember is omakase",
+        "body": "There are lots of à la carte software..."
+      }
+    ]
+  }
+};
+
+class MyInitialRenderBenchmark extends InitialRenderBenchmark {
+  port: number;
+  constructor(name: string, index: number) {
+    let port = DEFAULT_PORT + index;
+    super({
+      name,
+      url: `http://localhost.twitch.tv:${port}/lirik`,
+      markers: [
+        { start: "domLoading", label: "load" },
+      ],
+      browser: browserOpts
+    });
+    this.port = port;
+  }
+
+  async warm(tab: ITab): Promise<void> {
+    let id = await tab.addScriptToEvaluateOnLoad(`
+      let data = JSON.parse(${JSON.stringify(DATA)});
+
+      Object.keys(data).forEach(function(key) {
+        window.localStorage.setItem(key, JSON.stringify(data[key]));
+      });
+
+      document.cookie = "/index-${this.name}.html";
+    `);
+
+    await tab.navigate(`http://localhost.twitch.tv:${this.port}/404`, true);
+
+    await new Promise(resolve => setTimeout(resolve, 2500));
+
+    await tab.removeScriptToEvaluateOnLoad(id);
+
+    await tab.navigate(`http://localhost.twitch.tv:${this.port}/?cached`, true);
+
+    await new Promise(resolve => setTimeout(resolve, 2500));
+
+    await tab.navigate(`http://localhost.twitch.tv:${this.port}/?tracing`, true);
+  }
+}
+
+new Runner(process.argv.slice(2).map((exp, i) => new MyInitialRenderBenchmark(exp, i)))
+.run(50)
+.then(results => {
+  results.forEach(result => {
+    fs.writeFileSync(`results/${result.set}.json`, JSON.stringify(result))
+  });
+}).catch(err => {
+  console.error(err.stack);
+  process.exit(1);
+});
